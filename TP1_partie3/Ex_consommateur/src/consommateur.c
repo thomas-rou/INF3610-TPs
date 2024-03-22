af6976dc2edf450b34b6da433a1767174c70de91
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-03-22 00:04:42 -0400

    1st implementation

---
+++ TP1_partie3/Ex_consommateur/src/consommateur.c
@@ -0,0 +1,253 @@
+/*
+*********************************************************************************************************
+*                                                 uC/OS-III
+*                                          The Real-Time Kernel
+*                                               PORT Windows
+*
+*
+*            		          					Guy BOIS
+*                                  Polytechnique Montreal, Qc, CANADA
+*                                                 
+*
+*
+*********************************************************************************************************
+*/
+
+#include  <cpu.h>
+#include  <lib_mem.h>
+
+#include <os.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdbool.h>
+
+#include <app_cfg.h>
+#include <cpu.h>
+#include <ucos_bsp.h>
+#include <ucos_int.h>
+#include <xparameters.h>
+#include <KAL/kal.h>
+
+#include <xil_printf.h>
+
+#include  <stdio.h>
+#include  <ucos_bsp.h>
+
+#include <Source/os.h>
+#include <os_cfg_app.h>
+
+#include <xgpio.h>
+#include <xintc.h>
+#include <xil_exception.h>
+
+XIntc axi_intc;
+XGpio gpSwitch;
+
+#define GIC_DEVICE_ID	        XPAR_PS7_SCUGIC_0_DEVICE_ID
+#define PL_INTC_IRQ_ID          XPS_IRQ_INT_ID
+#define FIT_1S_IRQ_ID           XPAR_AXI_INTC_0_FIT_TIMER_0_INTERRUPT_INTR
+#define FIT_3S_IRQ_ID			XPAR_AXI_INTC_0_FIT_TIMER_1_INTERRUPT_INTR
+#define GPIO_SW_IRQ_ID			XPAR_AXI_INTC_0_AXI_GPIO_0_IP2INTC_IRPT_INTR
+#define GPIO_SW_DEVICE_ID		XPAR_AXI_GPIO_0_DEVICE_ID
+
+#define XGPIO_IR_MASK		0x3 /**< Mask of all bits */
+
+
+/*
+*********************************************************************************************************
+*                                              CONSTANTS
+*********************************************************************************************************
+*/
+
+// Attention ce programme loge à l'adresse 0x2000000 et possede une dimension de 0x1000000
+// Vérifier lsscript.ld
+
+// Comme consommateur n est pas maitre (UCOS_AMP_MASTER doit etre a false)
+
+// Attention ce programma va partager avec consommateur.c une zone de mémoire
+const uint32_t 	BASEADDR = 0x3000000;
+
+
+#define         TASK_STK_SIZE    8192                // Size of each task's stacks (# of WORDs)
+
+#define 		TASK_CONSOMMATEUR_PRIO	  15   				// Priorité de TaskActive
+
+/*
+*********************************************************************************************************
+*                                              VARIABLES
+*********************************************************************************************************
+*/
+
+
+CPU_STK           TaskConsommateurStk[TASK_STK_SIZE];
+CPU_STK 		  StartupTaskStk[UCOS_START_TASK_STACK_SIZE];
+
+OS_TCB			  TaskConsommateurTCB;
+OS_TCB 			  StartupTaskTCB;
+
+/*
+ *********************************************************************************************************
+*                                         FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+void    TaskConsommateur(void *data);
+void 	StartupTask (void *p_arg);
+
+/*
+*********************************************************************************************************
+*                                                  MAIN
+*********************************************************************************************************
+*/
+
+int main (void)
+{
+
+ 	OS_ERR  err;
+
+    UCOS_LowLevelInit();
+
+    CPU_Init();
+    Mem_Init();
+    OSInit(&err);
+
+    OSTaskCreate(&StartupTaskTCB,"Main Task", StartupTask, (void *) 0, UCOS_START_TASK_PRIO,
+                 &StartupTaskStk[0], 0, UCOS_START_TASK_STACK_SIZE,
+                 0, 0, DEF_NULL, (OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR), &err);
+
+	OSTaskCreate(&TaskConsommateurTCB,"TaskConsommateur", TaskConsommateur, (void *) 0, TASK_CONSOMMATEUR_PRIO,
+       &TaskConsommateurStk[0u], TASK_STK_SIZE/2, TASK_STK_SIZE, 20, 0, (void *) 0,
+	   (OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR), &err);
+
+	Xil_DCacheDisable();
+
+    OSStart(&err);
+    return 0;                                         // Start multitasking
+}
+
+void  TaskConsommateur (void *p_arg)
+{
+    OS_ERR       os_err;
+
+	// Variables partagees entre core 0 et core 1 (shared memory de 3 mots de 32 bits à partir de BASEADDR)
+	// Les 2 premiers mots servent à la synchronisation et le dernier au data echange
+	// Attention ack de TaskProducteur doit être jumelé avec req de TaskConsommateur
+	//        et req de TaskProducteur doit être jumelé avec ack de TaskConsommateur
+
+	volatile uint32_t* req = (uint32_t*) (BASEADDR +0x4);	// signal comme quoi on est prêt à recevoir un data
+	volatile uint32_t* ack = (uint32_t*) (BASEADDR +0x0);	// signal comme quoi on attend que le producteur est ecrit un data
+	volatile uint32_t* ptr = (uint32_t*) (BASEADDR +0x8);	// data partagee
+
+
+	xil_printf ("Task Consommateur sur core 0\r\n");
+
+//	*req = 0;
+//	*ack = 0;
+
+
+ while (DEF_TRUE) {
+
+    *req = 1;        								// Je suis pret a consommer
+
+    while(!*ack);   								// J'attends que le producteur est ecrit une donnee
+
+	xil_printf("Je viens de recevoir la valeur de i: %d\n\r", *ptr) ;
+
+	*req = 0;        								// J'ai fini de consommer
+
+    while(*ack);									// J'attends que le producteur me dise de passer à une prochaine valeur à consommer
+
+	OSTimeDlyHMSM(0, 0, 0, 10, OS_OPT_TIME_HMSM_STRICT, &os_err);       // Dans le lab 1, on peut supposer que c'est le delai pour vider les fifos...
+
+}
+
+}
+
+
+/*
+*********************************************************************************************************
+*                                               STARTUP TASK
+*********************************************************************************************************
+*/
+
+void StartupTask (void *p_arg)
+{
+	int i;
+	OS_ERR err;
+    KAL_ERR kal_err;
+    CPU_INT32U tick_rate;
+
+#if (UCOS_START_DEBUG_TRACE == DEF_ENABLED)
+    MEM_SEG_INFO seg_info;
+    LIB_ERR lib_err;
+#endif
+#if (APP_OSIII_ENABLED == DEF_ENABLED)
+#if (OS_CFG_STAT_TASK_EN == DEF_ENABLED)
+    OS_ERR  os_err;
+#endif
+#endif
+
+
+    UCOS_IntInit();
+
+#if (APP_OSIII_ENABLED == DEF_ENABLED)
+    tick_rate = OS_CFG_TICK_RATE_HZ;
+#endif
+
+#if (APP_OSII_ENABLED == DEF_ENABLED)
+    tick_rate = OS_TICKS_PER_SEC;
+#endif
+
+    UCOS_TmrTickInit(tick_rate);                                /* Configure and enable OS tick interrupt.              */
+
+#if (APP_OSIII_ENABLED == DEF_ENABLED)
+#if (OS_CFG_STAT_TASK_EN == DEF_ENABLED)
+    OSStatTaskCPUUsageInit(&os_err);
+#endif
+#endif
+
+    KAL_Init(DEF_NULL, &kal_err);
+    UCOS_StdInOutInit();
+    UCOS_PrintfInit();
+
+
+#if (UCOS_START_DEBUG_TRACE == DEF_ENABLED)
+    xil_printf("UCOS - uC/OS Init Started.\r\n");
+    xil_printf("UCOS - STDIN/STDOUT Device Initialized.\r\n");
+#endif
+
+#if (APP_SHELL_ENABLED == DEF_ENABLED)
+    UCOS_Shell_Init();
+#endif
+
+#if ((APP_FS_ENABLED == DEF_ENABLED) && (UCOS_CFG_INIT_FS == DEF_ENABLED))
+    UCOS_FS_Init();
+#endif
+
+#if ((APP_TCPIP_ENABLED == DEF_ENABLED) && (UCOS_CFG_INIT_NET == DEF_ENABLED))
+    UCOS_TCPIP_Init();
+#endif /* (APP_TCPIP_ENABLED == DEF_ENABLED) */
+
+#if ((APP_USBD_ENABLED == DEF_ENABLED) && (UCOS_CFG_INIT_USBD == DEF_ENABLED) && (UCOS_USB_TYPE == UCOS_USB_TYPE_DEVICE))
+    UCOS_USBD_Init();
+#endif /* #if (APP_USBD_ENABLED == DEF_ENABLED) */
+
+#if ((APP_USBH_ENABLED == DEF_ENABLED) && (UCOS_CFG_INIT_USBH == DEF_ENABLED) && (UCOS_USB_TYPE == UCOS_USB_TYPE_HOST))
+    UCOS_USBH_Init();
+#endif /* #if (APP_USBH_ENABLED == DEF_ENABLED) */
+
+#if (UCOS_START_DEBUG_TRACE == DEF_ENABLED)
+    Mem_SegRemSizeGet(DEF_NULL, 4, &seg_info, &lib_err);
+
+    xil_printf ("UCOS - UCOS init done\r\n");
+    xil_printf ("UCOS - Total configured heap size. %d\r\n", seg_info.TotalSize);
+    xil_printf ("UCOS - Total used size after init. %d\r\n", seg_info.UsedSize);
+#endif
+
+    xil_printf("Programme initialise - \r\n");
+    xil_printf("Frequence courante du tick d horloge - %d\r\n", tick_rate);
+
+    OSTaskSuspend((OS_TCB *)0,&err);
+
+}
+
---
+++ TP1_partie3/lab1_partie3_A0/src/interruptions.c
@@ -0,0 +1,182 @@
+/*
+ *********************************************************************************************************
+ *											  Interrupts
+ 
+ *											  
+ *********************************************************************************************************
+ */
+#include "interrupts.h"
+
+#include "routeur.h"
+
+
+void initialize_gpio0()
+{
+	if (XST_DEVICE_NOT_FOUND == XGpio_Initialize(&gpButton, GPIO_BUTTON_DEVICE_ID))
+		UCOS_Print("Erreur init gpio\n");
+	XGpio_SetDataDirection(&gpButton, 1, 0x1);
+	XGpio_SetDataDirection(&gpButton, 2, 0x0);
+	XGpio_InterruptGlobalEnable(&gpButton);
+	XGpio_InterruptEnable(&gpButton, XGPIO_IR_MASK);
+
+}
+
+void initialize_gpio1()
+{
+	if (XST_DEVICE_NOT_FOUND == XGpio_Initialize(&gpSwitch, GPIO_SWITCH_DEVICE_ID))
+		UCOS_Print("Erreur init gpio\n");
+	XGpio_SetDataDirection(&gpSwitch, 1, 0x1);
+	XGpio_SetDataDirection(&gpSwitch, 2, 0x0);
+	XGpio_InterruptGlobalEnable(&gpSwitch);
+	XGpio_InterruptEnable(&gpSwitch, XGPIO_IR_MASK);
+}
+
+void initialize_timer()
+{
+	/* ******************************************************************************* */
+	// Setup HW timer
+int status;
+
+	status = XTmrCtr_Initialize(&timer_dev, XPAR_AXI_TIMER_DEVICE_ID);
+	if(status != XST_SUCCESS)
+	{
+		xil_printf("Error: timer setup failed\n");
+		//return XST_FAILURE;
+	}
+	XTmrCtr_SetResetValue(&timer_dev, XPAR_AXI_TIMER_DEVICE_ID, 0xFFFFFFFF);
+	XTmrCtr_SetOptions(&timer_dev, XPAR_AXI_TIMER_DEVICE_ID, XTC_INT_MODE_OPTION | XTC_AUTO_RELOAD_OPTION | XTC_DOWN_COUNT_OPTION );
+
+}
+
+int initialize_axi_intc() {
+	int status;
+
+	status = XIntc_Initialize(&axi_intc, XPAR_AXI_INTC_0_DEVICE_ID);
+	if (status != XST_SUCCESS)
+		return XST_FAILURE;
+
+	return XST_SUCCESS;
+}
+
+int connect_fit_timer_irq0() {
+	int status;
+
+	status = XIntc_Connect(&axi_intc, FIT_IRQ0_ID, fit_timer_isr0, NULL);
+		if (status != XST_SUCCESS)
+			return status;
+
+	XIntc_Enable(&axi_intc, FIT_IRQ0_ID);
+
+	return XST_SUCCESS;
+
+
+}
+
+
+int connect_fit_timer_irq1() {
+	int status;
+
+	status = XIntc_Connect(&axi_intc, FIT_IRQ1_ID, fit_timer_isr1, NULL);
+		if (status != XST_SUCCESS)
+			return status;
+
+	XIntc_Enable(&axi_intc, FIT_IRQ1_ID);
+
+	return XST_SUCCESS;
+}
+
+int connect_timer_irq() {
+	int status;
+
+	status = XIntc_Connect(&axi_intc, TIMER_IRQ_ID, timer_isr, NULL);
+		if (status != XST_SUCCESS)
+			return status;
+
+	XIntc_Enable(&axi_intc, TIMER_IRQ_ID);
+
+	return XST_SUCCESS;
+}
+
+
+int connect_gpio_irq0()
+{
+	int status = XIntc_Connect(&axi_intc, GPIO_BUTTON_IRQ_ID, gpio_isr0, &gpButton);
+	if (status == XST_SUCCESS)
+		XIntc_Enable(&axi_intc, GPIO_BUTTON_IRQ_ID);
+	return status;
+}
+
+int connect_gpio_irq1()
+{
+
+	int status = XIntc_Connect(&axi_intc, GPIO_SWITCH_IRQ_ID, gpio_isr1, &gpSwitch);
+	if (status == XST_SUCCESS)
+		XIntc_Enable(&axi_intc, GPIO_SWITCH_IRQ_ID);
+	return status;
+
+}
+
+
+void connect_axi() {
+
+	// La connexion est sur IRQ ID#31
+	CPU_BOOLEAN succes = UCOS_IntVectSet (PL_INTC_IRQ_ID,
+			                             1,
+			                             0,
+										 (UCOS_INT_FNCT_PTR)XIntc_DeviceInterruptHandler,
+										 (void*)(uint32_t)axi_intc.CfgPtr->DeviceId);
+	if (succes != DEF_OK)
+		UCOS_Print ("connect axi : FAIL \n");
+	succes = UCOS_IntSrcEn(PL_INTC_IRQ_ID);
+	if (succes != DEF_OK)
+		UCOS_Print ("enable axi : FAIL \n");
+
+	connect_gpio_irq0();
+	connect_gpio_irq1();
+	connect_fit_timer_irq0();
+	connect_fit_timer_irq1();
+	connect_timer_irq();
+	XIntc_Start(&axi_intc, XIN_REAL_MODE);
+
+
+}
+
+void cleanup() {
+	/*
+	 * Disconnect and disable the interrupt
+	 */
+
+	disconnect_intc_irq();
+	disconnect_fit_timer_irq1();
+	disconnect_fit_timer_irq0();
+	disconnect_timer_irq();
+}
+
+void disconnect_intc_irq() {
+	UCOS_IntSrcDis(PL_INTC_IRQ_ID);
+}
+
+void disconnect_fit_timer_irq0() {
+	/*
+	 * Disconnect and disable the interrupt
+	 */
+	XIntc_Disable(&axi_intc, FIT_IRQ1_ID);
+	XIntc_Disconnect(&axi_intc, FIT_IRQ1_ID);
+}
+void disconnect_fit_timer_irq1() {
+	/*
+	 * Disconnect and disable the interrupt
+	 */
+	XIntc_Disable(&axi_intc, FIT_IRQ1_ID);
+	XIntc_Disconnect(&axi_intc, FIT_IRQ1_ID);
+
+}
+
+void disconnect_timer_irq() {
+	/*
+	 * Disconnect and disable the interrupt
+	 */
+	XIntc_Disable(&axi_intc, TIMER_IRQ_ID);
+	XIntc_Disconnect(&axi_intc, TIMER_IRQ_ID);
+
+}
